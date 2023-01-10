e3dc3fcc43199a34c0693c01c3d7e8ef22ae5b99
suleyman <gameclub2015@mail.ru>
2023-01-10 12:35:48 +0500

    ejs degisiklikleri

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
+const ejs = require("ejs");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  const blogum = (blog = {
-    id: 1,
-    title: "Blog title",
-    description: "Blog description",
-  });
-  res.send(blogum);
+  res.render("index");
+});
+
+app.get("/home", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/post", (req, res) => {
+  res.render("post");
 });
 
 const port = 3000;
