39a884d2ada71e7c55f31b630b3d22622991294e
suleyman <gameclub2015@mail.ru>
2023-01-11 22:32:47 +0500

    mongose schema data base

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const ejs = require("ejs");
+const Photo = require("./models/Photo");
+
+// connect db
+mongoose.connect("mongodb://127.0.0.1:27017/cleanblog-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 app.set("view engine", "ejs");
+
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({});
+  res.render("index", {
+    photos,
+  });
+});
+
+app.post("/photos", async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect("/");
+});
 
 app.get("/", (req, res) => {
   res.render("index");
